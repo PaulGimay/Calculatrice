8fbf94e2e348511261122bc8c5f035c24cf718b8
PaulGimay <81556930+PaulGimay@users.noreply.github.com>
2021-05-06 12:19:18 +0200

    Responsive et fichier .JS

---
+++ calc.js
@@ -0,0 +1,18 @@
+const myNumbers = document.querySelectorAll('div[data-button="chiffre"]');
+myNumbers.forEach(element => {
+    element.addEventListener('click', function () {
+        resultat.value += this.dataset.translate // += va me permettre la concaténation.
+    })
+});
+const myResult = document.querySelectorAll('div[data-button="egal"]');
+myResult.forEach(element => {
+    element.addEventListener('click', function () {
+        resultat.value = eval(resultat.value)
+    })
+});
+const clear = document.querySelectorAll('div[data-button="cancel"]');
+clear.forEach(element => {
+    element.addEventListener('click', function () {
+        resultat.value = ""
+    })
+});
